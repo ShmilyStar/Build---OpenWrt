dee99fdb09bd780c3da3b0d4ba2655c9cb2f4ee9
ShmilyStar <92926599+ShmilyStar@users.noreply.github.com>
2024-04-22 10:10:42 +0800

    Delete redmi-ax6000 directory

--- redmi-ax6000/1.modify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# modify login IP
-sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# modify login password to 888888
-#sed -i '/root/croot:$1$qIM0M9Qt$1b8gHF7Bi2aUIbRq1FHy50:19835:0:99999:7:::' package/base-files/files/etc/shadow
